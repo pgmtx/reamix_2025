48f1483f857e551e1a317b2c7ad3037bfdef8d5b
eric-lefebvre <eric@lefebvre-famille.fr>
2026-01-12 11:58:45 +0100

    fix push d'avant

--- Assets/_Scripts/Balles/Balles.cs
+++ Assets/_Scripts/Balles/Balles.cs
@@ -1,11 +1,22 @@
-using UnityEngine;
+ using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Balles : MonoBehaviour
 {
     public bool isAlreadyInBasket = false;
+    public GameEvent BallPickedUp;
+    bool ballPickedUpTriggered;
 
     void Update()
     {
-        GetComponent<XRGrabInteractable>().isSelected;
+        if (ballPickedUpTriggered)
+        {
+            return;
+        }
+
+        if (GetComponent<XRGrabInteractable>().isSelected)
+        {
+            BallPickedUp.TriggerEvent();
+        }
     }
 }
