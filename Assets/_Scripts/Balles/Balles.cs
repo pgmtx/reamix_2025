5b10b8cb4005af434bb0fe932b4aa67e518bc354
eric-lefebvre <eric@lefebvre-famille.fr>
2026-01-12 11:40:50 +0100

    save

--- Assets/_Scripts/Balles/Balles.cs
+++ Assets/_Scripts/Balles/Balles.cs
@@ -3,4 +3,9 @@ using UnityEngine;
 public class Balles : MonoBehaviour
 {
     public bool isAlreadyInBasket = false;
+
+    void Update()
+    {
+        GetComponent<XRGrabInteractable>().isSelected;
+    }
 }
