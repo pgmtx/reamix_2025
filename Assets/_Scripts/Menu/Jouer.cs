7c9d7180eeeb1390cffca56184c3e167f6c47eb1
Mateo.O <mateo.olivier72@gmail.com>
2026-02-11 10:52:45 +0100

    Ajout jouer.cs + gestion des lumières WIP

--- Assets/_Scripts/Menu/Jouer.cs
+++ Assets/_Scripts/Menu/Jouer.cs
@@ -1,18 +1,44 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.XR.Interaction.Toolkit;
 
 public class Jouer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    private XRSimpleInteractable interactable;
+
+    public GameObject eclairage;
+
+    public GameObject menu;
+
+    public GameObject barriere;
+    private XRGrabInteractable barriereInteractable;
+
+    public GameObject spotlight;
+
+    void Awake()
     {
-        
+        // On desactive les interactions avec la barriere
+        barriereInteractable = barriere.GetComponent<XRGrabInteractable>();
+        barriereInteractable.enabled = false;
+
+        // Eclairage eteint
+        eclairage.SetActive(false);
+        spotlight.SetActive(true);
+
+        // Mettre l'interaction sur le bouton Jouer
+        interactable = GetComponent<XRSimpleInteractable>();
+        interactable.selectEntered.AddListener(OnPressed);
     }
 
-    // Update is called once per frame
-    void Update()
+    void OnPressed(SelectEnterEventArgs args)
     {
-        
+        Debug.Log("Le bouton Jouer a ete presse !");
+
+        // On desac le menu et remet les valeurs a defaut
+        menu.SetActive(false);
+        eclairage.SetActive(true);
+        spotlight.SetActive(false);
+        barriereInteractable.enabled = true;
     }
 }
