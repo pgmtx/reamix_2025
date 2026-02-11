13bd8769aa1f557d0748ef6380792ddcb8d5c783
Eric Lefebvre <eric@lefebvre-famille.fr>
2026-02-11 18:07:09 +0100

    upgrade oeil tele, images, suivi

--- AGENTS.md
+++ AGENTS.md
@@ -8,7 +8,7 @@
   - Layers:
     - Default, TransparentFX, Ignore Raycast, Water, UI, Ground
 - Active game object:
-  - Name: XR Origin (XR Rig)
-  - Tag: Player
-  - Layer: Ignore Raycast
+  - Name: TV
+  - Tag: Untagged
+  - Layer: Default
 <!-- UNITY CODE ASSIST INSTRUCTIONS END -->
--- Assets/_Scripts/TV/TVEye.cs
+++ Assets/_Scripts/TV/TVEye.cs
@@ -7,16 +7,16 @@ public class TVEye : MonoBehaviour
 {
     [SerializeField] private Transform player;
     [SerializeField] private Slider outerEye;
+    [SerializeField] private Slider middleEye;
     [SerializeField] private Slider innerEye;
-    [Range(-10f, 0f)]
-    [SerializeField] private float playerMinX;
-    [Range(-0f, 10f)]
+    [Range(0f, 10f)]
     [SerializeField] private float playerMaxX;
 
     private void Update()
     {
         float playerCurrentX = player.position.x;
         outerEye.value = 0.5f + (playerCurrentX / (2 * playerMaxX));
-        innerEye.value = 0.5f + (playerCurrentX / (2 * -playerMinX));
+        middleEye.value = 0.5f + (playerCurrentX / (2 * playerMaxX));
+        innerEye.value = 0.5f + (playerCurrentX / (2 * playerMaxX));
     }
 }
