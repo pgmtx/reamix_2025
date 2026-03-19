13b534fedea3eac45c29436f3003406b85cef49b
eric-lefebvre <eric@lefebvre-famille.fr>
2026-03-19 11:30:37 +0100

    Essai retour haptic marteau

--- Assets/_Scripts/Whack_a_mole/MarteauBehaviour.cs
+++ Assets/_Scripts/Whack_a_mole/MarteauBehaviour.cs
@@ -4,19 +4,50 @@ using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
 
 [RequireComponent(typeof(XRGrabInteractable))]
-public class MarteauBehaviour : MonoBehaviour
+public class MarteauBehaviour : StaticInstance<MarteauBehaviour>
 {
     [SerializeField] private GameEvent marteauPickedUp;
     private XRGrabInteractable xrGrabInteractable;
+
+    [Header("Haptic Feedback")]
+    [Range(0f, 1f)]
+    public float hapticFeedbackIntensity;
+    [Range(0f, 1f)]
+    public float hapticFeedbackDuration;
+    [ReadOnly]
+    public XRBaseController ControllerHoldingMarteau;
+
     private void Awake()
     {
         xrGrabInteractable = GetComponent<XRGrabInteractable>();
-
         xrGrabInteractable.selectEntered.AddListener(OnSelectEnter);
+        xrGrabInteractable.selectExited.AddListener(OnSelectExit);
     }
 
-    void OnSelectEnter(SelectEnterEventArgs args)
+    public void OnSelectEnter(BaseInteractionEventArgs eventArgs)
+    {
+        if (eventArgs.interactorObject is XRBaseControllerInteractor controllerInteractor)
+        {
+            OnSelectEnter(controllerInteractor.xrController);
+        }
+    }
+
+    public void OnSelectEnter(XRBaseController controller)
     {
         marteauPickedUp.TriggerEvent();
+        ControllerHoldingMarteau = controller;
+    }
+
+    public void OnSelectExit(BaseInteractionEventArgs eventArgs)
+    {
+        if (eventArgs.interactorObject is XRBaseControllerInteractor controllerInteractor)
+        {
+            OnSelectExit(controllerInteractor.xrController);
+        }
+    }
+
+    public void OnSelectExit(XRBaseController controller)
+    {
+        ControllerHoldingMarteau = null;
     }
 }
